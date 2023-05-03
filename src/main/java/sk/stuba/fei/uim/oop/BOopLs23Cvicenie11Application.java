94362d0882dc664c5a6c5bc1908a6876afdd3760
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-05-03 10:40:12 +0200

    init commit

---
+++ src/main/java/sk/stuba/fei/uim/oop/BOopLs23Cvicenie11Application.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BOopLs23Cvicenie11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BOopLs23Cvicenie11Application.class, args);
+    }
+
+}
